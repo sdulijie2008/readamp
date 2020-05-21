a29b7381e63a9712eafce2a07df4a8ce27853901
Juraj Slugen <jslugen@microsoft.com>
2020-05-20 22:56:54 -0700

    Add duration information to Loop script.

--- LoopBlobsWithRead.ps1
+++ LoopBlobsWithRead.ps1
@@ -1,7 +1,7 @@
 $blobsCount = 20
-$today = Get-Date
+$executionStart = Get-Date
 For ($i=0; $i -le 20; $i++) {
-	$blobPrefix = [string]::format("blob-{0}-{1:D2}-{2:D2}-{3:D2}:{4:D2}_{5:X8}-", $today.Year, $today.Month, $today.Day, $today.Minute, $today.Hour, $i)
+	$blobPrefix = [string]::format("blob-{0}-{1:D2}-{2:D2}-{3:D2}:{4:D2}_{5:X8}-", $executionStart.Year, $executionStart.Month, $executionStart.Day, $executionStart.Hour, $executionStart.Minute, $i)
 	Write-output "`r`nGoing to upload and read back $blobsCount blobs with prefix $blobPrefix."
 	cmd /c "dotnet run --project ./storage-blob-dotnet-latency-profiler/storage-blob-dotnet-latency-profiler.csproj testcontainer connstring results.csv prod true PutRandomSizeBlobTest $blobPrefix $blobsCount 268435456 100 99"
     cmd /c "dotnet run --project ./storage-blob-dotnet-latency-profiler/storage-blob-dotnet-latency-profiler.csproj testcontainer connstring results.csv prod true DataValidationSequenceTest $blobPrefix $blobsCount 268435456 100 100 99"
@@ -11,3 +11,5 @@ For ($i=0; $i -le 20; $i++) {
 	}
 	Write-output "`r`nFinished uploading and validation of $blobsCount blobs with prefix $blobPrefix.`r`n"
 }
+$executionEnd = Get-Date
+Write-output ("`r`nScript started at {0}, ended at {1} and took {2}.`r`n" –f $executionStart.ToString(), $executionEnd.ToString(), ($executionEnd - $executionStart).ToString())
